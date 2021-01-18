313978d4f9a4135f0f6e4bbeadd60c5fb4b5aa4b
gofff <khatiullin@gmail.com>
2021-01-18 20:04:42 +0300

    Add tests to PathRequester's helper functions

--- tests/test_path_requester.py
+++ tests/test_path_requester.py
@@ -1,44 +1,121 @@
+import sys
+sys.path.append('./yddg')
+
 import pytest
-from yddg.path_requester import PathRequester
 import multiprocessing as mp
 
 
-def test_object_init():
-
-    path_requester = PathRequester()
+import yddg.path_requester as path_requester
 
 
-class TestPathRequesterAllPaths:
+CORRECT_URL = 'https://yadi.sk/d/FMbYkNAfcOYAzg?w=1'
+BAD_URL = ''
+CORRECT_OUT_PATHS = ['/folder_1/avo.jpg', 
+                     '/folder_1/file_2.txt', 
+                     '/folder_2/file_3.txt', 
+                     '/folder_2/file_4.txt', 
+                     '/file_1.txt']
+
+@pytest.fixture
+def correct_public_test_data():
+    return {
+            'url': CORRECT_URL,
+            'max_files': 1000
+           }
 
 
-    def test_get_all_paths_method_empty_input(self):
+def test_correct_out_YD_request_items(correct_public_test_data):
 
-        path_requester = PathRequester()
-        out = path_requester.get_all_paths([])
-        expected = []
-        assert out == expected
-
-
-class TestPathRequesterStreamPaths:
-
-
-    def test_get_path_stream_method_noout(self):
-
-        path_queue = mp.Queue(1)
-        path_requester = PathRequester()
-        out = path_requester.get_path_stream([], path_queue)
-        assert out is None
+    url = correct_public_test_data['url']
+    max_files = correct_public_test_data['max_files']
+    out = path_requester.YD_request_items(url, '', max_files)
+    
+    assert out is not None
+    assert len(out)
+    assert len(out[0]['name'])
+    assert out[0]['type'] in ['dir', 'file']
 
 
-    def test_get_path_stream_method_out_queue(self):
+@pytest.fixture(params=[
+        {
+            'url': CORRECT_URL,
+            'max_files': 0
+        },
+        {
+            'url': BAD_URL,
+            'max_files': 1000
+        },
+])
+def bad_public_test_data(request):
+    return request.param
 
-        path_queue = mp.Queue(1)
-        val = 0
-        path_queue.put(val, block = True)
+def test_bad_out_YD_request_items(bad_public_test_data):
 
-        path_requester = PathRequester()
-        path_requester.get_path_stream([], path_queue)
-        
-        out = path_queue.get(block = True)
-        assert out == val
+    url = bad_public_test_data['url']
+    max_files = bad_public_test_data['max_files']
+    out = path_requester.YD_request_items(url, '', max_files)
+    
+    assert out is not None
+    assert not len(out)
+
+
+@pytest.fixture
+def correct_test_item():
+    return [{
+            'type': 'dir',
+            'path': ''
+            }]
+
+
+@pytest.fixture(params = [
+        {
+            'type': 'unknown',
+            'path': ''
+        },
+        {
+            'type': 'dir',
+            'path': 'not_exist_path/'
+        }
+    ])
+def bad_test_item(request):
+    return [request.param]
+
+
+@pytest.fixture
+def correct_out_paths():
+    return CORRECT_OUT_PATHS
+
+
+def test_correct_out_YD_get_required_files(correct_test_item,
+                                           correct_public_test_data,
+                                           correct_out_paths):
+
+    url = correct_public_test_data['url']
+    max_files = correct_public_test_data['max_files']
+    out = path_requester.YD_get_required_files(correct_test_item,
+                                               url, max_files)
+
+    assert len(out)
+    # Have not empty strings
+    assert not len([p for p in out if len(p) == 0])\
+
+
+def test_bad_out_YD_get_required_files(bad_test_item,
+                                       bad_public_test_data,
+                                       correct_out_paths):
+
+    url = bad_public_test_data['url']
+    max_files = bad_public_test_data['max_files']
+    out = path_requester.YD_get_required_files(bad_test_item,
+                                               url, max_files)
+
+    assert not len(out)
+
+
+
+
+
+
+
+
 
