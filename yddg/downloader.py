2a887a9955e2d57173e1a7227602bec3288248fb
gofff <khatiullin@gmail.com>
2021-01-21 15:10:43 +0300

    Add implementation of Downloader methods

--- yddg/downloader.py
+++ yddg/downloader.py
@@ -2,6 +2,8 @@ import requests
 from typing import List, Tuple, Any
 import multiprocessing as mp
 
+import constants as const
+
 
 class Downloader:
 
@@ -11,18 +13,45 @@ class Downloader:
         return
 
 
-    def download_file(self, link: str, path: str) -> Any:
+    def download_file(self, url: str, path: str) -> bytes:
 
-        return 0
+        api_url = const.YD_API.PUBLIC_DOWNLOAD_URL
+        params = {'public_key': url}
+        if not path:
+            params['path'] = path
+
+        resp = requests.get(api_url, params)
+        if resp.status_code != requests.codes.ok:
+            const.bad_request_warning(resp.status_code,
+                                      api_url, params)
+            return b''
+        download_url = resp.json()['href']
+
+        resp = requests.get(download_url)
+        if resp.status_code != requests.codes.ok:
+            const.bad_request_warning(resp.status_code,
+                                      download_url, params = '')
+            return b''
+
+        return resp.content
 
 
-    def download_stream(self, paths: List[Tuple[str, str]],
+    def download_stream(self, download_paths: List[Tuple[str, str]],
                         out_queue: mp.Queue) -> None:
 
+        for url, path in download_paths:
+            file_bytes = self.download_file(url, path)
+            out_queue.put((url, path, file_bytes), block = True)
+        out_queue.put(None, block = True)
         return
 
 
     def download_stream_from_stream(self, path_queue: mp.Queue,
                                     out_queue: mp.Queue) -> None:
-
+        download_path = path_queue.get(block = True)
+        while download_path is not None:
+            url, path = download_path
+            file_bytes = self.download_file(url, path)
+            out_queue.put((url, path, file_bytes), block = True)
+        out_queue.put(None, block = True)
         return
