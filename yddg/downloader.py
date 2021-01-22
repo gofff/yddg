d5bcacd20317350edbf5ff5d5ee784319b4970f8
gofff <khatiullin@gmail.com>
2021-01-22 20:23:16 +0300

    Fix logic mistakes

--- yddg/downloader.py
+++ yddg/downloader.py
@@ -2,7 +2,7 @@ import requests
 from typing import List, Tuple, Any
 import multiprocessing as mp
 
-import constants as const
+import yddg.constants as const
 
 
 class Downloader:
@@ -17,9 +17,8 @@ class Downloader:
 
         api_url = const.YD_API.PUBLIC_DOWNLOAD_URL
         params = {'public_key': url}
-        if not path:
+        if path:
             params['path'] = path
-
         resp = requests.get(api_url, params)
         if resp.status_code != requests.codes.ok:
             const.bad_request_warning(resp.status_code,
@@ -53,5 +52,6 @@ class Downloader:
             url, path = download_path
             file_bytes = self.download_file(url, path)
             out_queue.put((url, path, file_bytes), block = True)
+            download_path = path_queue.get(block = True)
         out_queue.put(None, block = True)
         return
