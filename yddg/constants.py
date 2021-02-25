8d84f333ecb1dac867341dd68c91a87d00e2d7d2
gofff <khatiullin@gmail.com>
2021-02-25 18:02:42 +0300

    Fix unexpected indent error

--- yddg/constants.py
+++ yddg/constants.py
@@ -6,15 +6,14 @@ import requests
 
 class YD_API:
 
-    PUBLIC_URL = 'https://cloud-api.yandex.net'
-                 + '/v1/disk/public/resources'
+    PUBLIC_URL = 'https://cloud-api.yandex.net/v1/disk/public/resources'
     PUBLIC_DOWNLOAD_URL = PUBLIC_URL + '/download'
 
 
 def bad_request_warning(status_code: int, url: str, params: Any) -> None:
 
     warnings.warn(
-            f'Bad request status ({status_code}) to ' 
-            + f'{url} with params: {params}',
-            RuntimeWarning
-        )
+        f'Bad request status ({status_code}) to '
+        + f'{url} with params: {params}',
+        RuntimeWarning,
+    )
