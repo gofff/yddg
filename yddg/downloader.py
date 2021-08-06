4c364b10356328db3a44169a3732e5a6f9c1118a
gofff <khatiullin@gmail.com>
2021-08-06 13:47:25 +0300

    Reimplement downloader with async

--- yddg/downloader.py
+++ yddg/downloader.py
@@ -1,55 +1,74 @@
 import multiprocessing as mp
 from typing import List, Tuple
 
+import aiohttp
+import asyncio
 import requests
 
-import yddg.constants as const
+import constants as const
+from path_requester import PathRequester
 
 
-def download_file(url: str, path: str) -> bytes:
+async def download_file(session: aiohttp.ClientSession, url: str,
+                        path: str) -> bytes:
 
+    download_url = ""
     api_url = const.YD_API.PUBLIC_DOWNLOAD_URL
-    params = {'public_key': url}
-    if path:
-        params['path'] = path
-    resp = requests.get(api_url, params)
-    if resp.status_code != requests.codes.ok:
-        const.bad_request_warning(resp.status_code, api_url, params)
-        return b''
-    download_url = resp.json()['href']
+    params = {
+                'public_key': url,
+                'path': path,
+             }
+    async with session.get(api_url, params=params) as resp:
+        if resp.status != const.REQ_STATUS.OK:
+            const.bad_request_warning(resp.status, api_url, params)
+            return b''
 
-    resp = requests.get(download_url)
-    if resp.status_code != requests.codes.ok:
-        const.bad_request_warning(resp.status_code,
-                                  download_url,
-                                  params='')
-        return b''
+        resp_json = await resp.json()
+        download_url = resp_json['href']
 
-    return resp.content
+    async with session.get(download_url) as resp:
+        if resp.status != const.REQ_STATUS.OK:
+            const.bad_request_warning(resp.status, download_url, params="")
+            return b''
+        return await resp.content.read()
+
+    assert f"Wrong return statement in download_file({url}, {path})"
+    return b''
 
 
-class Downloader:
+async def download_task(path_queue: asyncio.Queue, 
+                        out_queue: asyncio.Queue) -> None:
+    
+    async with aiohttp.ClientSession() as session:
+        url_path = await path_queue.get()
+        path_queue.task_done()
+        while url_path is not None:
+            content = await download_file(session, *url_path)
+            await out_queue.put((*url_path, content))
+            url_path = await path_queue.get()
+            path_queue.task_done()
+        await out_queue.put(None)
+        
+'''
+async def download_consumer(file_q):
+    item = await file_q.get()
+    file_q.task_done()
+    while item is not None:
+        print(f"downloaded: {item[0]} {item[1]}")
+        item = await file_q.get()
+        file_q.task_done()
 
-    def __init__(self) -> None:
+async def test_downloader():
+    path_q = asyncio.Queue()
+    file_q = asyncio.Queue()
+    pr = PathRequester(['https://yadi.sk/d/FMbYkNAfcOYAzg?w=1'], 1000, path_q)
+    await asyncio.create_task(pr.run())
+    await asyncio.create_task(download_task(path_q, file_q))
+    await asyncio.create_task(download_consumer(file_q))
+    await path_q.join()
+    await file_q.join()
 
-        return
 
-    def download_stream(self, download_paths: List[Tuple[str, str]],
-                        out_queue: mp.Queue) -> None:
-
-        for url, path in download_paths:
-            file_bytes = download_file(url, path)
-            out_queue.put((url, path, file_bytes), block=True)
-        out_queue.put(None, block=True)
-        return
-
-    def download_stream_from_stream(self, path_queue: mp.Queue,
-                                    out_queue: mp.Queue) -> None:
-        download_path = path_queue.get(block=True)
-        while download_path is not None:
-            url, path = download_path
-            file_bytes = download_file(url, path)
-            out_queue.put((url, path, file_bytes), block=True)
-            download_path = path_queue.get(block=True)
-        out_queue.put(None, block=True)
-        return
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test_downloader())
+'''
