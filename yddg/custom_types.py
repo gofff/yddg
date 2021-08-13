cea913436b310641313a8058d3ab39371fd7a536
gofff <khatiullin@gmail.com>
2021-08-13 14:22:21 +0300

    Fix issue with generic class types
      With python versions < 3.9 classes that are generic
      in type declarations lead to an error

--- yddg/custom_types.py
+++ yddg/custom_types.py
@@ -1,7 +1,18 @@
 import asyncio
-from typing import Optional, Tuple
+from typing import TYPE_CHECKING, Optional, Tuple
 
 YDiskPath = Tuple[str, str]
-ExtractTask = Optional[asyncio.Future[None]]
-YDiskPathQueue = asyncio.Queue[Optional[YDiskPath]]
-ItemQueue = asyncio.Queue[Optional[Tuple[str, str, bytes]]]
+
+# TYPE_CHEKING need to avoid
+#   TypeError: 'type' object is not subscriptable
+# for generic classes in stubs but not at runtime
+# fi look at:
+# https://mypy.readthedocs.io/en/stable/runtime_troubles.html#runtime-troubles
+if TYPE_CHECKING:
+    ExtractTask = Optional[asyncio.Future[None]]
+    YDiskPathQueue = asyncio.Queue[Optional[YDiskPath]]
+    ItemQueue = asyncio.Queue[Optional[Tuple[str, str, bytes]]]
+else:
+    ExtractTask = asyncio.Future
+    YDiskPathQueue = asyncio.Queue
+    ItemQueue = asyncio.Queue
